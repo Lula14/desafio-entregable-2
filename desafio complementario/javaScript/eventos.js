0c0f935c4de72207b871001720544f4ff0ea8dec
Maria L Iglesias <106163736+Lula14@users.noreply.github.com>
2022-08-24 22:02:45 -0300

    Add files via upload

--- desafio complementario/javaScript/eventos.js
+++ desafio complementario/javaScript/eventos.js
@@ -1,5 +1,6 @@
-let boton = document.getElementById("botonAgregar")
-boton.addEventListener("click", respuestaClick)
+const boton = () => {document.querySelectorAll(".botonAgregar")
+boton.addEventListener("click", respuestaClick)}
 function respuestaClick() {
     console.log("Se ha agregado un producto")
 }
+respuestaClick()
